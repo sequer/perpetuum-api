bc9cde0c7195152bda13f991e68feb1e8d3b65c1
Bart Brinkman <bart@instantmagazine.com>
2018-03-27 00:35:19 +0200

    add small primer to emails currently sent

--- module/Onboarding/src/V1/Rpc/Register/RegisterController.php
+++ module/Onboarding/src/V1/Rpc/Register/RegisterController.php
@@ -112,8 +112,8 @@ class RegisterController extends AbstractActionController
                     'name' => 'Open Perpetuum Team',
                     'email' => 'no-reply@mail.openperpetuum.com',
                 ],
-                'subject' => 'Activate your account',
-                'html' => '<html><body><p>Hi there! Click <a href="http://register.openperpetuum.com/activate/?token={{token}}">here</a> to activate your account.</p></body></html>',
+                'subject' => 'Activate your account [no action required]',
+                'html' => '<html><body><p>Hi there! You\'ve recently registered an account on the Open Perpetuum server. Click <a href="http://register.openperpetuum.com/#/activate/{{token}}">here</a> to activate your Open Perpetuum account.</p><p>PS: We are currently testing our registration system. Unfortunately, our mailing sandbox got lost and we\'re sending real emails! There\'s no need to do anything with this email now. After the testing period is over, you will probably receive an email like this one after registering asking you to confirm your email address. This will be primarely used to give you a way to recover a lost password. Thanks!</p></body></html>',
             ],
             'substitution_data' => [
                 'token' => $token->getHash(),
